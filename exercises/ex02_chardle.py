97869c0e208591d1d0b24b86e2f849f4a32443ad
Ashley Seung <aseung@ad.unc.edu>
2024-09-25 09:45:34 -0400

    Progress on Exercise 3

--- exercises/ex02_chardle.py
+++ exercises/ex02_chardle.py
@@ -7,10 +7,11 @@ def input_word() -> str:
     user_selected_word = str(input("Enter a 5-character word: "))
     if len(user_selected_word) == 5:
         return user_selected_word
+        # Struggled to understand significance of a return statement on line 9.
     else:
         print("Error: Word must contain 5 characters.")
         exit()
-    return user_selected_word
+        # exit() should be placed on line 13 because this is only if the len(user_selected_word) != 5
 
 
 def input_letter() -> str:
@@ -19,33 +20,47 @@ def input_letter() -> str:
         return user_selected_letter
     else:
         print("Error: character must be a single character.")
-        return user_selected_letter
+        exit()
 
 
 def contains_char(word: str, letter: str) -> None:
     count: int = 0
-    print("Searching for " + str(letter) + "in " + str(word))
+    # Code did not run because I did not declare count's type and initial value.
+    print("Searching for " + letter + " in " + word)
+    # Code does not need str(letter) or str(word) because they were already defined as str.
     if word[0] == letter:
-        print(str(letter) + " found at index 0")
+        print(letter + " found at index 0")
+        count += 1
+        # Must remember to redefine the value of count when the condition of the if statement is true.
     if word[1] == letter:
-        print(str(letter) + " found at index 1")
+        print(letter + " found at index 1")
+        count += 1
     if word[2] == letter:
-        print(str(letter) + " found at index 2")
+        print(letter + " found at index 2")
+        count += 1
     if word[3] == letter:
-        print(str(letter) + " found at index 3")
+        print(letter + " found at index 3")
+        count += 1
     if word[4] == letter:
-        print(str(letter) + " found at index 4")
-        if count == 0:
-            print("No instances of " + str(letter) + "found in " + str(word))
-        elif count == 1:
-            print("1 instance of " + str(letter) + "found in " + str(word))
-        else:
-            print(str(count) + " instances of " + str(letter) + "found in " + str(word))
+        print(letter + " found at index 4")
+        count += 1
+
+    if count == 0:
+        print("No instances of " + letter + " found in " + word)
+        # Needed to remember that strings can concanate together.
+    elif count == 1:
+        print("1 instance of " + letter + " found in " + word)
+    else:
+        print(str(count) + " instances of " + letter + " found in " + word)
+        # count needs to be turned into a str because it used to be an int.
 
 
 def main() -> None:
-    contains_char(word=input_word)
-    letter = input_letter
+    word = input_word()
+    letter = input_letter()
+    contains_char(word, letter)
+    # Struggled in how to call the different function definitions.
+    # Line 62 helps call back the input_word and input_letter function definitions.
 
 
 if __name__ == "__main__":
---
+++ exercises/ex03_wordle.py
@@ -0,0 +1,79 @@
+"""Implementing Advanced Strings to Imitate Wordle."""
+
+__author__ = "730749279"
+
+
+def input_guess(secret_word: int) -> str:
+    """"""
+    while True:
+        # By having the condition True, the while loop continues on.
+        guess = input(f"Enter a {secret_word} character word: ")
+        if len(guess) == secret_word:
+            return guess
+            # By returning guess, the while loop is not entered anymore.
+        else:
+            print(f"That wasn't {secret_word} chars! Try again: ")
+
+
+def contains_char(secret_word: str, char_guess: str) -> bool:
+    """Identifying if a character exists within the selected word."""
+    assert len(char_guess) == 1
+    # Line 20 ensures the character guess is one character only.
+    idx = 0
+    while idx < len(secret_word):
+        if secret_word[idx] == char_guess:
+            return True
+        idx += 1
+    else:
+        return False
+
+
+def emojified(guess: str, secret: str) -> str:
+    """Displays the audience a string of emojis to show if their guess is on par with the secret word."""
+    assert len(guess) == len(secret)
+    # Line 32 ensures the length of the chosen guess and secret are the same.
+    WHITE_BOX: str = "\U00002B1C"
+    GREEN_BOX: str = "\U0001F7E9"
+    YELLOW_BOX: str = "\U0001F7E8"
+    # In lines 35-37, the variable's initial values do not alter.
+    emoji_display = ""
+    # Just as the idx was initalized at 0, the emoji_display is a string.
+    # So, its initalized value is "".
+    while idx < len(guess):
+        if guess[idx] == secret[idx]:
+            emoji_display += GREEN_BOX
+            # We do not redefine emoji_display, rather we add on to its initalized value.
+        elif contains_char(secret, guess[idx]):
+            emoji_display += YELLOW_BOX
+        else:
+            emoji_display += WHITE_BOX
+        idx += 1
+        # Line 49 ensures all characters of the guess are looped through.
+    return emoji_display
+
+
+def main(secret: str) -> None:
+    """Bringing together functions to handle the progression of the game."""
+    allowed_turns = 6
+    turn = 1
+    # Initalize the turn variable with 1, because that is their first attempt.
+    while turn <= allowed_turns:
+        print(f" === Turn {turn}/{allowed_turns} ===")
+        guess = input_guess(len(secret))
+        # Line 62 calls back the input_guess function.
+        result = emojified(guess, secret)
+        print(result)
+        # Line 65 calls back the emojified function.
+        # Now, we need to create code for when users find the secret_word.
+        if guess == secret:
+            print(f"You won in {turn}/{allowed_turns} turns!")
+            return
+            # Line 70 allows the user to exit the game after winning.
+        turn += 1
+        # Line 72 adds on to the turns until turn <= allowed_turns is False.
+    if turn > allowed_turns:
+        print(f"X/{allowed_turns} - Sorry, try again tomorrow!")
+
+
+if __name__ == "__main__":
+    main(secret="codes")
